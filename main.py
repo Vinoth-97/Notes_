221e7f47e2531d666417b0395ca1456189e8daac
navinravikumar98 <101760954+navinravikumar98@users.noreply.github.com>
2022-04-10 12:30:36 +0100

    Adding the Docker Files
    Adding the Docker Files Done by vinoth

--- main.py
+++ main.py
@@ -1,6 +1,15 @@
 from website import create_app
 
+# app = create_app()
+
+# if __name__ == '__main__':
+#     app.run(debug=True)
+import os
+# from website import create_app
+
 app = create_app()
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    server_port = os.environ.get('PORT', '8080')
+    app.run(debug=False, port=server_port, host='0.0.0.0')
+
